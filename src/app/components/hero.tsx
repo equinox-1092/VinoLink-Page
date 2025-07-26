c9cb70be4e3436c5fd6646655aa10015a01b5867
equinox-1092 <sdiaz1234di@gmail.com>
2025-07-25 22:11:17 -0500

    feat(hero-section): add content information about the name of product

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export const Hero = () => {
   return (
-    <div className="min-h-screen flex flex-col md:flex-row items-center justify-center p-4 md:p-8 max-w-7xl mx-auto w-full">
+    <div className="flex flex-col md:flex-row items-center justify-center p-4 md:p-8 max-w-7xl mx-auto w-full animate-fade-in-down animate-duration-1000">
       {/* Text Content */}
       <div className="w-full md:w-1/2 text-center md:text-left p-4">
         <h1 className="text-7xl md:text-7xl font-bold py-7">
@@ -16,7 +16,7 @@ export const Hero = () => {
           <p className="text-lg md:text-xl">
             Join the VinoLink community and start connecting with wineries today!
           </p>
-          <p className="text-gray-500 dark:text-gray-400">
+          <p className="text-gray-500 dark:text-gray-400 italic">
             The best tool at your disposal for perfect business management.
           </p>
         </div>
