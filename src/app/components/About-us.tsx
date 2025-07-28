5a8a9a437b35a6021b2fda9b7078b77ce1157cc1
equinox-1092 <sdiaz1234di@gmail.com>
2025-07-27 23:07:08 -0500

    fix: correction in the text about us

--- src/app/components/About-us.tsx
+++ src/app/components/About-us.tsx
@@ -14,7 +14,7 @@ export const AboutUs = () => {
       <div className="w-full md:w-1/2 space-y-6">
         <h1 className="text-4xl font-bold">About Us</h1>
         <p className="text-lg">
-          Vino Link is the platform that unites producers, distributors, and
+          Vino Link is a platform that unites producers, distributors, and
           wine enthusiasts in a collaborative environment. Winegrowers showcase
           their wineries and harvests, distributors manage catalogs and orders,
           and users receive personalized recommendations and share reviews. Each
